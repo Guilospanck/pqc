a8369b34930c21628660e186767265987ed5643c
Guilherme Pereira <guilospanck@protonmail.com>
2026-01-12 13:26:13 +0000

    Add event handler

---
+++ tui/src/event-handler.ts
@@ -0,0 +1,60 @@
+type Topic =
+  | "exit"
+  | "update_message_area"
+  | "send_message"
+  | "update_input_bar"
+  | "add_message";
+
+type SubscriberId = string;
+
+type Subscriber = {
+  id: SubscriberId;
+  callback: Function;
+};
+
+interface IEventHandler {
+  subscribe(topic: Topic, subscriber: Subscriber): void;
+  unsubscribe(id: SubscriberId, topic: Topic): void;
+  notify(topic: Topic): void;
+}
+
+export const EventHandler = (() => {
+  let singleton: IEventHandler | null = null;
+
+  return () => {
+    if (singleton) return singleton;
+
+    const subscriptions: Map<Topic, Set<Subscriber>> = new Map();
+
+    singleton = {
+      subscribe(topic, subscriber) {
+        const currentSubs = subscriptions.get(topic);
+        if (!currentSubs) {
+          subscriptions.set(topic, new Set([subscriber]));
+          return;
+        }
+
+        currentSubs.add(subscriber);
+        subscriptions.set(topic, currentSubs);
+      },
+      unsubscribe(id, topic) {
+        const currentSubs = subscriptions.get(topic);
+        if (!currentSubs) return;
+
+        const filtered = Array.from(currentSubs).filter(
+          (item) => item.id !== id,
+        );
+
+        subscriptions.set(topic, new Set(filtered));
+      },
+      notify(topic) {
+        const currentSubs = subscriptions.get(topic);
+        if (!currentSubs) return;
+
+        Array.from(currentSubs).forEach((sub) => sub.callback());
+      },
+    };
+
+    return singleton;
+  };
+})();
